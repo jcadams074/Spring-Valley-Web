14fa8164f482135726344acd7f98f45214a6e677
Jacob Adams <jcadams074@gmail.com>
2024-12-18 17:48:06 -0600

    Removed unused import in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./common-layout/header/header.component";
 import { FooterComponent } from "./common-layout/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
